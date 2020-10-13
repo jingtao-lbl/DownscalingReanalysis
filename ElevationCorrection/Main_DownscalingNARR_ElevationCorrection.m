8cffe48968acb9933a6a6993f77d4297eefb1805
jingtao-lbl <48232131+jingtao-lbl@users.noreply.github.com>
2020-10-13 14:18:53 -0700

    Update Main_DownscalingNARR_ElevationCorrection.m

--- ElevationCorrection/Main_DownscalingNARR_ElevationCorrection.m
+++ ElevationCorrection/Main_DownscalingNARR_ElevationCorrection.m
@@ -8,7 +8,7 @@
 %% Generating NARR fields at 3hr1km resolution
 % For air temperature, air pressure, specific humidity and downward longwave radiation
 % 1. Read and extract NARR original fields
-% 2. Calculating dynamic lapse rate 
+% 2. Calculating dynamic lapse rates 
 % 3. Adjusting NARR fields for elevation correction 
 % for iyear=2012:2017
 NARR_SEUS_Adj_3hr1km(iyear);
